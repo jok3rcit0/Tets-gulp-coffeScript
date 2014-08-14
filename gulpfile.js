22178f301e2bb46ffd8fc62c620d6ca27378c498
Juan Herrera <jok3rcit0>
2014-08-14 06:15:26 -0500

    minificar js con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@
 var gulp 	= require('gulp');
 var uglify 	= require('gulp-uglify');
 
+//asignar rutas a variables
+var coffeO 	= 'js/coffe/*.js';
+var jsDes 	= 'js/';
+
 //crear una tarea
 gulp.task('scripts', function(){
-	console.log('funciona');
+	//trae todo los .coffe y comprime a 1 fichero minificado
+	return gulp.src(coffeO)
+		.pipe(uglify('main.min.js'))
+		.pipe(gulp.dest(jsDes));
 });
