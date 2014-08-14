3b55cc4cb884c51d2fd1ac826ac06ad1f0f85c15
Juan Herrera <jok3rcit0>
2014-08-14 00:45:56 -0500

    Prueba uno gulp task

---
+++ README.md
@@ -0,0 +1,8 @@
+Aprendiendo a usar gulp y coffescript
+          ~Bushidō~
+
+
+Para hacer funcionar este ejemplo sigue los pasos, uan vez clonado el repo:
+
+1.- npm install
+2.- gulp scripts
---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+//llamar a los modulos
+var gulp 	= require('gulp');
+var uglify 	= require('gulp-uglify');
+
+//crear una tarea
+gulp.task('scripts', function(){
+	console.log('funciona');
+});
